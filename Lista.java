7c4dfe2614db0fcbc590a793b600bbfc1d83a6c2
Simone-Gramuglia <simone.gramuglia@marconibari.edu.it>
2026-02-08 17:20:24 +0000

    consegna

--- Lista.java
+++ Lista.java
@@ -1,1 +1,24 @@
-//Implementa qui la classe Lista
+public class Lista <T>{
+    private Nodo <T> head;
+    public Lista (){
+        this.head = null;
+    }
+    public boolean isEmpty(){
+        return head == null;
+    }
+
+    public void aggiungiInTesta(T dato){
+        Nodo<T> nuovoNodo = new Nodo<>(dato);
+        nuovoNodo.next = head;
+        head = nuovoNodo;
+    }
+
+    public void aggiungiInCoda(T dato){
+        Nodo<T> nuovoNodo = new Nodo<>(dato);
+        if(head == null){
+            head = nuovoNodo;
+            return;
+        }
+        
+    }
+}
